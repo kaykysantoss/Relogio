e19289dbf3ecf7551e4cd0183d81e37eb84d3697
kaykysantoss <132955705+kaykysantoss@users.noreply.github.com>
2023-05-16 23:09:01 -0300

    first commit

---
+++ Relogio/index.js
@@ -0,0 +1,18 @@
+const Horas = document.getElementById('horas')
+const Minutos = document.getElementById('minutos')
+const Segundos = document.getElementById('segundos')
+
+const relogio =  setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (seg < 10) seg = '0' + seg;
+
+    Horas.textContent = hr;
+    Minutos.textContent = min;
+    Segundos.textContent = seg;
+})
